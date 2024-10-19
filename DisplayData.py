0331e20b3443ab49f0aa5a4294138276f91e6507
Jacopo00811 <jacopo.ceccuti@gmail.com>
2024-10-19 11:08:33 +0200

    Refactored code and added splits for dataset PH2

---
+++ DisplayData.py
@@ -0,0 +1,66 @@
+from Dataloader import *
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+
+transform = transforms.Compose(
+    [
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+    ]
+)
+
+drive_dataset = DRIVE(train=True, transform=transform)
+drive_loader = torch.utils.data.DataLoader(drive_dataset, batch_size=1, shuffle=True)
+
+for index, (images, masks) in enumerate(drive_loader):
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.title("Image")
+    plt.imshow(images[0].permute(1, 2, 0))  # Convert from CHW to HWC
+    plt.axis("off")
+
+    plt.subplot(1, 2, 2)
+    plt.title("Mask")
+    plt.imshow(masks[0].squeeze(), cmap="gray")
+    plt.axis("off")
+
+    # plt.show()
+    plt.savefig(f"drive_sample_{str(index)}.png")
+    break
+
+# Test PH2 data loader similarly
+ph2_dataset = PH2(train=True, transform=transform)
+ph2_loader = torch.utils.data.DataLoader(ph2_dataset, batch_size=1, shuffle=True)
+
+
+for index, (images, lesions, rois) in enumerate(
+    DataLoader(ph2_dataset, batch_size=1, shuffle=True)
+):
+
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 3, 1)
+    plt.title("Image")
+    plt.imshow(images[0].permute(1, 2, 0))  # Convert from CHW to HWC
+    plt.axis("off")
+
+    plt.subplot(1, 3, 2)
+    plt.title("Lesion Mask")
+    plt.imshow(lesions[0].squeeze(), cmap="gray")
+    plt.axis("off")
+
+    plt.subplot(1, 3, 3)
+    plt.title("ROI Masks")
+    for roi in rois[0]:
+        plt.imshow(roi.squeeze(), cmap="gray", alpha=0.5)  # Overlay ROIs
+    plt.axis("off")
+
+    plt.savefig(f"ph2_sample_{str(index)}.png")
+    break  # Remove this to load more images
+
+
+print(f"\nThe dataset DRIVE has: {len(drive_dataset)} images")
+print(f"The dataset PH2 has: {len(ph2_dataset)} images\n")
